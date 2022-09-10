e575c81bffbffc4111d46616f3044ce923bbdf5a
Alec Hansen <alecdhansen@Alecs-Air.lan>
2022-09-10 19:19:46 -0400

    finished styling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-function myFunction(element, color) {
-  element.style.backgroundColor = color;
-}
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equals = document.querySelector("[data-equals]");
+const allClear = document.querySelector("[data-all-clear]");
+const displayScreen = document.querySelector("[data-display-screen]");
+
+numberButtons.forEach(function (numberButton) {
+  console.log(numberButton);
+});
+
+operationButtons.forEach(function (operationButton) {
+  console.log(operationButton);
+});
+
+equals.addEventListener("click", function () {
+  alert("=");
+});
+allClear.addEventListener("click", function () {
+  alert("A/C");
+});
